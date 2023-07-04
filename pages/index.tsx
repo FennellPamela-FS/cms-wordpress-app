6f97c5fea17cd125842bed3bb79a443a33a60cc2
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-07-03 23:44:50 -0400

    Inital Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,3 +43,23 @@ export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
     revalidate: 10,
   }
 }
+
+// Get the first 20 posts from WordPress, ordered by the date
+export async function getAllPostsFromWordPress(preview) {
+  const data = await fetchAPI(`
+    query AllPosts {
+      posts(first: 20, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            title
+            excerpt
+            slug
+            date
+          }
+        }
+      }
+    }
+  `)
+
+  return data.posts
+}
