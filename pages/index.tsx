9dbf8b3dfc813a3f74dfd77fff594ab641091ea9
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-07-04 00:09:15 -0400

    add line 49 back

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,3 +44,22 @@ export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
   }
 }
 
+// Get the first 20 posts from WordPress, ordered by the date
+export async function getAllPostsFromWordPress(preview) {
+  const data = await fetchAPI(`
+    query AllPosts {
+      posts(first: 20, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            title
+            excerpt
+            slug
+            date
+          }
+        }
+      }
+    }
+  `)
+
+  return data.posts
+}
